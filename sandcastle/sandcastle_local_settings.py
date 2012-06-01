a185edac9a5556220e50eae73bf6b2660c88b066
Ben Alpert <spicyjalapeno@gmail.com>
2012-05-31 17:46:55 -0700

    sandcastle configuration
    Summary:
    This machine also needs to run youtube-export and I've also not yet
    symlinked sandcastle/media/castles to an appropriate EBS drive but this
    script should set up sandcastle properly on Ubuntu 11.10.
    
    I know you probably have no idea how sandcastle is set up but I don't
    think any of the other full-timers do either so you get to review!
    
    Reviewers: csilvers
    
    Reviewed By: csilvers
    
    Differential Revision: http://phabricator.khanacademy.org/D135

---
+++ sandcastle/sandcastle_local_settings.py
@@ -0,0 +1,10 @@
+# Local Settings
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+# sandcastle doesn't use this key so it doesn't actually need to be secret
+SECRET_KEY = 'aui-+9zudw#-j!e*71sw#^5b-g*@ob46k9ob+&+5-q%jh=kv(9'
+
+# sandcastle configuration
+SANDCASTLE_USER = 'Khan'
+SANDCASTLE_REPO = 'khan-exercises'
