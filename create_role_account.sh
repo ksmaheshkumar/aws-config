ad07e6393370fd3fc65f8bfc95434a9aa3698eca
Ben Alpert <spicyjalapeno@gmail.com>
2012-08-08 10:56:37 -0700

    Avoid saying echo so many times
    Summary: Avoid saying echo so many times. Is a wheredoc the same thing as a heredoc?
    
    Test Plan: Ran it, output still looks reasonable.
    
    Reviewers: csilvers
    
    Reviewed By: csilvers
    
    Differential Revision: http://phabricator.khanacademy.org/D550

--- create_role_account.sh
+++ create_role_account.sh
@@ -17,17 +17,21 @@ if [ -z "$ROLE" ]; then
   exit 1
 fi
 
+cat <<EOF
+
 # Add the user account
-echo sudo adduser --disabled-password $ROLE --gecos \"\"
+sudo adduser --disabled-password $ROLE --gecos ""
 
 # mkdir .ssh
-echo sudo -u $ROLE mkdir -p ~$ROLE/.ssh/
+sudo -u $ROLE mkdir -p ~$ROLE/.ssh/
 
 # Move .ssh/authorized_keys over
-echo sudo mv ~ubuntu/.ssh/authorized_keys ~$ROLE/.ssh/
-echo sudo chown -R $ROLE: ~$ROLE/.ssh/
-echo sudo chmod 600 ~$ROLE/.ssh/authorized_keys
+sudo mv ~ubuntu/.ssh/authorized_keys ~$ROLE/.ssh/
+sudo chown -R $ROLE: ~$ROLE/.ssh/
+sudo chmod 600 ~$ROLE/.ssh/authorized_keys
 
 # Add role account to sudoers
-echo echo "\"$ROLE ALL=(ALL) NOPASSWD:ALL\"" \| \
-  sudo tee -a /etc/sudoers.d/90-cloudimg-ubuntu \>/dev/null
+echo "$ROLE ALL=(ALL) NOPASSWD:ALL" | \\
+  sudo tee -a /etc/sudoers.d/90-cloudimg-ubuntu >/dev/null
+
+EOF
