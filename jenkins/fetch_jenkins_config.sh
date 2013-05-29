9d52b6038461e4ce1a420a5ca33efb35c1c7e766
Chris Klaiber <chris@khanacademy.org>
2013-05-29 11:51:52 -0700

    Add script to automate config fetching
    Summary:
    This script does what I used to do by hand -- it fetches the latest
    copies of all known configs from the Jenkins server. Then it's easy to
    see a text diff of the changes and commit them.
    
    Test Plan:
    Ran ./fetch_jenkins_config.sh and verified that the new parts of the
    server's configuration were copied locally. I then created other
    changesets out of those changes.
    
    Reviewers: csilvers
    
    Reviewed By: csilvers
    
    Differential Revision: http://phabricator.khanacademy.org/D2461

---
+++ jenkins/fetch_jenkins_config.sh
@@ -0,0 +1,37 @@
+#!/bin/sh -e
+
+# Fetch a subset of configuration from a remote Jenkins server, by default the
+# server is ka-jenkins.
+#
+# The motivation is that Jenkins tends to be configured via the web UI and
+# persists its own config into XML, XML that we want in source control to more
+# easily configure a new Jenkins instance. This script is meant to be run from
+# a local checkout of a source repository on a development machine and modifies
+# local files under ./jenkins_home/ to match the remote server's configuration.
+#
+# This works by gathering all the paths rooted in the local jenkins_home/
+# directory and using SSH to copy the corresponding files rooted at the $HOME
+# of the jenkins user on the ka-jenkins server.
+#
+# USAGE:
+#   $ ./fetch_jenkins_config.sh
+#   Fetching ka-jenkins:./config.xml
+#   Fetching ka-jenkins:./jobs/website-commit/config.xml
+#   ...
+#
+# To fetch a new file first create the local file then run the fetch script:
+#   $ touch jenkins_home/jobs/a-new-job/config.xml
+
+# TODO(chris): detect missing files that should be copied.
+
+# Make sure we're in the directory this script resides in.
+cd "`dirname $0`/jenkins_home"
+
+host=ka-jenkins
+remote_jenkins_home=/var/lib/jenkins
+
+for f in `find . -type f`
+do
+  echo "Fetching $host:$f"
+  ssh "$host" sudo -u jenkins "cat '$remote_jenkins_home/$f'" > "$f"
+done
