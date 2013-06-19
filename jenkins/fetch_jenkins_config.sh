2ec11db0f6e5a3f2a0eaea28c37f6f2fb333f693
Chris Klaiber <chris@khanacademy.org>
2013-06-18 17:15:10 -0700

    Sync (almost) all Jenkins config
    Summary:
    There are many changed files but the most important are
    fetch_jenkins_config.exclude and fetch_jenkins_config.sh
    
    The configuration files are mostly new files that we didn't previously
    track. Most of the config isn't useful, it is simply default values,
    with the exception of the global GIT user settings.
    
    Test Plan:
    I ran the new fetch script and verified that it picked up the config
    changes I expected (mostly just creating files we weren't previously
    tracking) and omitted files listed in the exclude file.
    
      $ ./fetch_jenkins_config.sh
    
    Reviewers: csilvers
    
    Reviewed By: csilvers
    
    CC: desmond
    
    Differential Revision: http://phabricator.khanacademy.org/D2718

--- jenkins/fetch_jenkins_config.sh
+++ jenkins/fetch_jenkins_config.sh
@@ -1,28 +1,18 @@
 #!/bin/sh -e
 
-# Fetch a subset of configuration from a remote Jenkins server, by default the
-# server is ka-jenkins.
+# Fetch XML configuration from a remote Jenkins server, by default the server
+# is ka-jenkins.
 #
-# The motivation is that Jenkins tends to be configured via the web UI and
-# persists its own config into XML, XML that we want in source control to more
-# easily configure a new Jenkins instance. This script is meant to be run from
-# a local checkout of a source repository on a development machine and modifies
-# local files under ./jenkins_home/ to match the remote server's configuration.
-#
-# This works by gathering all the paths rooted in the local jenkins_home/
-# directory and using rsync over SSH to copy the corresponding files rooted at
-# the $HOME of the jenkins user on the ka-jenkins server.
+# Jenkins is configured via the web UI and persists config into XML that we
+# want under source control. This script is meant to be run from a local
+# checkout of a source repository on a development machine and modifies local
+# files under ./jenkins_home/ to match the remote server's configuration.
 #
 # USAGE:
 #   $ ./fetch_jenkins_config.sh
 #   receiving file list ... done
 #   jobs/website-commit/config.xml
 #   ...
-#
-# To fetch a new file first create the local file then run the fetch script:
-#   $ touch jenkins_home/jobs/a-new-job/config.xml
-
-# TODO(chris): detect missing files that should be copied.
 
 # Make sure we're in the directory this script resides in.
 cd "`dirname $0`/jenkins_home"
@@ -30,5 +20,13 @@ cd "`dirname $0`/jenkins_home"
 host=ka-jenkins
 remote_jenkins_home=/var/lib/jenkins
 
-find . -type f -print0 \
-  | rsync -av -e ssh --rsync-path="sudo -u jenkins rsync" --files-from=- --from0 "$host":"$remote_jenkins_home" .
+# The include/exclude filters are non-obvious. See "FILTER RULES" in "man
+# rsync". Each filter is applied in order at each level of a recursive descent.
+# The idea is to sync all XML configuration in the home directory and jobs/*/
+# but to exclude files that we list in fetch_jenkins_config.exclude
+rsync -avk -e ssh --rsync-path="sudo -u jenkins rsync" \
+  --exclude-from=../fetch_jenkins_config.exclude \
+  --include='/jobs' --include='/jobs/*' --include='/jobs/*/*.xml' \
+  --include='/*.xml' \
+  --exclude='*' \
+  "$host":"$remote_jenkins_home"/ .
