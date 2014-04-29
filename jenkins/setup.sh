487bdeb033a11636a790c8f72062b2f99db1c23c
Dylan Vassallo <dylan@khanacademy.org>
2014-04-29 14:26:03 -0700

    Install ncurses headers in the Jenkins machine's setup script
    @johnsullivan made a devshell.py fix that requires the Python readline package
    to be installed. By installing the readline dependency ncurses-dev on Jenkins
    we can put readline in requirements.txt so that all devs will automatically
    get it as part of their setup process.
    
    Test Plan: On the Jenkins machine, ran the following commands without error:
    ```
    sudo apt-get update
    sudo apt-get install ncurses-dev
    pip install readline
    ```
    
    Auditors: johnsullivan

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -45,6 +45,7 @@ install_basic_packages() {
     echo "Installing basic packages"
     sudo apt-get install -y ntp
     sudo apt-get install -y curl
+    sudo apt-get install -y ncurses-dev
     sudo apt-get install -y python-pip
     sudo apt-get install -y python-dev  # for numpy
     sudo apt-get install -y git mercurial subversion
