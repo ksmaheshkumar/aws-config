c75175308fbb7e285ac3626624fdf38e221003aa
Craig Silverstein <csilvers@khanacademy.org>
2014-11-18 20:52:57 -0800

    Rename the script to refer to phabricator, not toby.
    The extra disk on the toby machine only holds phabricator data.  This
    is unlikely to change -- phabricator deserves its own disk, we'd be
    more likely to add a second disk if need be.  So update this script to
    make it clear it's phabricator data that's being backed up here.
    
    (I care because I'm moving phabricator off toby sometime soon, and
    don't want to keep the -- now misleading -- toby name when that
    happens.)
    
    Auditors: alpert
    
    Test Plan:
    Fingers crossed.  Will check tomorrow to make sure the nightly backup
    to s3 still happened.

--- internal-webserver/snapshot_toby.sh
+++ internal-webserver/snapshot_toby.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-# A script that makes a (rolling) snapshot of the data on toby.
+# A script that makes a (rolling) snapshot of data on the phabricator disk.
 #
 # This assumes that the volume holding toby's data has a tag named
-# 'Name' with value 'toby data ...'  And that this volume is an XFS
-# volume.
+# 'Name' with value 'phabricator data ...'  And that this volume is an
+# XFS volume.
 
 set -e       # die if any command fails
 
 VOLUME=`ec2-describe-volumes \
         -K ~/aws/pk-backup-role-account.pem \
         -C ~/aws/cert-backup-role-account.pem \
-        | grep -e 'TAG.*toby data' \
+        | grep -e 'TAG.*phabricator data' \
         | cut -f3`
 if [ `echo "$VOLUME" | wc -l` -ne 1 ]; then
-  echo "Cannot find a unique volume tagged with the name 'toby data'."
+  echo "Cannot find a unique volume tagged with the name 'phabricator data'."
   exit 1
 fi
 
@@ -22,7 +22,7 @@ PATH="$PATH":/usr/bin       # for ec2-*.
 
 "$HOME/aws-config/internal-webserver/ec2-create-rolling-snapshot.py" \
     -m 16 \
-    -d 'backup of toby data' \
+    -d 'backup of phabricator data' \
     -v "$VOLUME" \
     --freezedir=/opt \
     -K ~/aws/pk-backup-role-account.pem \
