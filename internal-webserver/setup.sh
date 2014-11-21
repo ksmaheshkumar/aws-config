35f5084cdc381be6c4520ef060e788deddb46996
Craig Silverstein <csilvers@khanacademy.org>
2014-11-20 16:55:04 -0800

    Move all the phabricator stuff to its own machine.
    Summary:
    Everything else on internal-webserver (toby) stays.  Mostly this
    involved moving files as appropriate, and splitting up setup.sh.  I
    also took the opportunity to audit install_phabricator() in setup.sh
    and fix some old stuff up.
    
    Have https://cr.khanacademy.org redirect to phabricator as well as http://.
    
    Also, fixed a few comments based on review.
    
    Test Plan:
    Fingers crossed.  Once this is landed I'll try creating a new
    machine on ec2.  (What I'll probably do is create a new toby machine,
    and turn the existing toby into phabricator.ka.org, since I don't want
    to worry about messing up phabricator.)
    
    (None new)
    
    Reviewers: alpert, chris
    
    Reviewed By: alpert, chris
    
    Differential Revision: http://phabricator.khanacademy.org/D14739

--- internal-webserver/setup.sh
+++ internal-webserver/setup.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 
 # This has files that are used on our internal webserver, that runs on
-# ec2.  It has things like the code review tools, the git mirrors,
-# etc.
+# ec2.  It has misc services that run for us, such as culture-cow.
 #
 # This sets up packages needed on an EC2 machine for
-# internal-webserver.  This is based on the Ubuntu11 AMI that Amazon
-# provides as one of the default EC2 AMI options.  It is idempotent.
+# internal-webserver.  This is based on an Ubuntu12.04 AMI.  It is
+# idempotent.
 #
 # This should be run in the home directory of the role account
 # of the user that is to run the internal webserver/etc.  That user
 # should be able to sudo to root.
-#
-# NOTE: This script, along with some of the data files in this
-# directory (notably etc/fstab), assume that a 'data' disk has been
-# attached to this ec2 instance as sdf (aka xvdf), and that's it's
-# been formatted with xfs.
 
 # Typically, this is run like this
 #
@@ -35,22 +29,6 @@ CONFIG_DIR="$HOME/aws-config/internal-webserver"
 . "$HOME/aws-config/shared/setup_fns.sh"
 
 
-install_ec2_tools() {
-    # Activate the multiverse!  Needed for ec2-api-tools
-    activate_multiverse       # from setup_fns.sh
-    sudo apt-get install -y ec2-api-tools
-    mkdir -p "$HOME/aws"
-    if [ ! -s "$HOME/aws/pk-backup-role-account.pem" ]; then
-        echo "Copy the pk-backup-role-account.pem and cert-backup-role-account.pem"
-        echo "files from dropbox to $HOME/aws:"
-        echo "   https://www.dropbox.com/home/Khan%20Academy%20All%20Staff/Secrets"
-        echo "Also, make sure there is an IAM user called 'backup-role-account"
-        echo "with permissions from 'backup-role-account-permissions'."
-        echo "Then hit enter to continue"
-        read prompt
-    fi
-}
-
 install_repositories() {
     echo "Syncing internal-webserver codebase"
     sudo apt-get install -y git
@@ -60,28 +38,13 @@ install_repositories() {
         ( cd internal-webserver && git pull && git submodule update --init --recursive )
 }
 
-install_root_config_files_toby() {
-    echo "Updating config files on the root filesystem (using symlinks)"
-    # This will fail (causing the script to fail) if python isn't
-    # python2.7.  If that happens, update the symlinks below to point
-    # to the right directory.
-    expr "`python --version 2>&1`" : "Python 2\.7" >/dev/null
-    sudo ln -snf "$HOME/internal-webserver/python-hipchat/hipchat" \
-                 /usr/local/lib/python2.7/dist-packages/
-
-    # Rest is standard'
-    install_root_config_files    # from setup_fns.sh
-}
-
 install_user_config_files() {
-    echo "Creating logs directory (for webserver logs)"
-    sudo mkdir -p /opt/logs
-    sudo chmod 1777 /opt/logs
-    sudo chown www-data.www-data /opt/logs
-    ln -snf /opt/logs "$HOME/logs"
-    ln -snf /var/log/nginx/error.log "$HOME/logs/nginx-error.log"
+    echo "Creating logs directory (for misc service logs)"
+    sudo mkdir -p $HOME/logs
+    sudo chmod 1777 $HOME/logs
 }
 
+# TODO(csilvers): do we still need this?
 install_appengine() {
     if [ -d "/usr/local/google_appengine" ]; then
         ( cd /usr/local/frankenserver && sudo git pull && sudo git submodule update --init --recursive )
@@ -92,112 +55,6 @@ install_appengine() {
           sudo ln -snf frankenserver/python google_appengine
         )
     fi
-}
-
-install_phabricator() {
-    echo "Installing packages: Phabricator"
-    sudo mkdir -p /opt/mysql_data
-    ln -snf /opt/mysql_data "$HOME/mysql_data"
-    sudo apt-get install -y git mercurial
-    sudo apt-get install -y make
-    # The envvar here keeps apt-get from prompting for a password.
-    # TODO(csilvers): should we give the root mysql user a password?
-    sudo env DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
-    sudo apt-get install -y php5 php5-mysql php5-cgi php5-fpm php5-gd php5-curl
-    sudo apt-get install -y libpcre3-dev php-pear    # needed to install apc
-    # php is dog-slow without APC
-    pecl list | grep -q APC || yes "" | sudo pecl install apc
-    sudo pip install pygments                      # for syntax highlighting
-    sudo ln -snf /usr/local/bin/pygmentize /usr/bin/
-    mkdir -p "$HOME/internal-webserver/phabricator/support/bin"
-    cat <<EOF >"$HOME/internal-webserver/phabricator/support/bin/README"
-Instead of putting binaries that phabricator needs in your $PATH, you can
-put symlinks to them here.  (This directory is in your phabricator-path.)
-EOF
-    if [ ! -s "/usr/lib/php5/20090626/xhprof.so"]; then
-        ( cd /var/tmp
-          rm -rf xhprof-0.9.2.tgz xhprof-0.9.2
-          wget http://pecl.php.net/get/xhprof-0.9.2.tgz
-          tar xfz xhprof-0.9.2.tgz
-          rm xhprof-0.9.2.tgz
-          cd xhprof-0.9.2/extension
-          phpize
-          ./configure
-          make
-          sudo make install
-        )
-    fi
-    echo "SELECT User FROM mysql.user" \
-        | mysql --user=root mysql \
-        | grep -q phabricator \
-    || echo "CREATE USER 'phabricator'@'localhost' IDENTIFIED BY 'codereview'; GRANT ALL PRIVILEGES ON *.* TO 'phabricator'@'localhost' WITH GRANT OPTION;" \
-        | mysql --user=root mysql
-
-    # Just in case phabricator is already running (we want to be idempotent!)
-    PHABRICATOR_ENV=khan "$HOME/internal-webserver/phabricator/bin/phd" stop
-    sudo service nginx stop
-    sudo service php5-fpm stop
-
-    env PHABRICATOR_ENV=khan \
-        internal-webserver/phabricator/bin/storage --force upgrade
-
-    # TODO(csilvers): automate entering this info:
-    cat <<EOF
-Enter this info:
-    * username: admin
-    * real name: Admin Admin
-    * email: toby-admin+phabricator@khanacademy.org
-    * password: <see secrets.py>
-    * system agent: n
-    * admin: y
-EOF
-    env PHABRICATOR_ENV=khan internal-webserver/phabricator/bin/accountadmin
-
-    # Set up the security token
-    "$HOME/internal-webserver/arcanist/bin/arc" install-certificate \
-        http://phabricator.khanacademy.org/api/
-
-    # We store the repositories on ephemeral disk since they're easy
-    # to re-create if need be.
-    sudo mkdir -p /mnt/phabricator/repositories
-    sudo chmod -R a+rX /mnt/phabricator
-    sudo chown -R ubuntu /mnt/phabricator
-    ln -snf /mnt/phabricator/repositories "$HOME/phabricator/repositories"
-    python "$HOME/internal-webserver/update_phabricator_repositories.py" -v \
-        "$HOME/phabricator/repositories"
-
-    # But we store the files on the same disk as mysql so that they get backed
-    # up alongside the database data
-    sudo mkdir -p /opt/phabricator_files
-    sudo chmod -R a+rX /opt/phabricator_files
-    sudo chown -R www-data /opt/phabricator_files
-    ln -snf /opt/phabricator_files "$HOME/phabricator/files"
-
-    # Start the daemons.
-    sudo mkdir -p /var/tmp/phd/log
-    sudo chown -R ubuntu /var/tmp/phd
-    sudo service php5-fpm start
-    sudo service nginx start
-    PHABRICATOR_ENV=khan "$HOME/internal-webserver/phabricator/bin/phd" start
-
-    # TODO(csilvers): automate this.
-    cat <<EOF
-To finish phabricator installation:
-1) Visit http://phabricator.khanacademy.org, sign in as admin, visit
-   http://phabricator.khanacademy.org/auth/config/new/, and follow
-   the directions to enable Google OAuth.
-2) Sign out, then sign in again via oauth, create a new account for
-   yourself, sign out, sign in as admin, and visit
-   http://phabricator.khanacademy.org/people/edit/2/role/
-   to make yourself an admin.
-3) On AWS's route53 (or whatever), add phabricator-files.khanacademy.org
-   as a CNAME to phabricator.khanacademy.org.
-4) Copy ka-wild-13.* from
-   https://www.dropbox.com/home/Khan%20Academy%20All%20Staff/Secrets
-   to /etc/nginx (as root) and chmod 600 /etc/nginx/ka-wild-13.*.
-EOF
-echo "Hit enter when this is done:"
-read prompt
 }
 
 install_gae_default_version_notifier() {
@@ -272,8 +129,9 @@ install_gae_dashboard() {
 }
 
 install_khantube_ouath_collector() {
-    # A simple flask server that will collect youtube oauth credentials needed
-    # inorder to upload captions to the various youtube accounts
+    # A simple flask server that will collect youtube oauth
+    # credentials needed in order to upload captions to the various
+    # youtube accounts
     sudo pip install -r "${HOME}"/internal-webserver/khantube-oauth-collector/requirements.txt
     sudo update-rc.d -f khantube-oauth-collector-daemon remove
     sudo ln -snf "${HOME}"/aws-config/internal-webserver/etc/init.d/khantube-oauth-collector-daemon /etc/init.d
@@ -288,7 +146,8 @@ install_khantube_ouath_collector() {
 }
 
 install_exercise_icons() {
-    # A utility to generate exercise icons. Currently used at http://khanacademy.org/commoncore/map.
+    # A utility to generate exercise icons. Currently used at
+    # http://khanacademy.org/commoncore/map.
     # https://github.com/Khan/exercise-icons/
     sudo apt-get install -y gcc-multilib xdg-utils libxml2-dev libcurl4-openssl-dev imagemagick
 
@@ -330,25 +189,15 @@ install_exercise_icons() {
     )
 }
 
-start_daemons() {
-    echo "Starting daemons in $HOME/aws-config/internal-webserver/etc/init"
-    for daemon in $HOME/aws-config/internal-webserver/etc/init/*.conf; do
-        sudo stop `basename $daemon .conf` || true
-        sudo start `basename $daemon .conf`
-    done
-}
-
 cd "$HOME"
 
 update_aws_config_env     # from setup_fns.sh
 install_basic_packages    # from setup_fns.sh
-install_ec2_tools
 install_repositories
-install_root_config_files_toby
+install_root_config_files # from setup_fns.sh
 install_user_config_files
 install_nginx             # from setup_fns.sh
 install_appengine
-install_phabricator
 install_gae_default_version_notifier
 install_culture_cow
 install_beep_boop
@@ -357,10 +206,9 @@ install_khantube_ouath_collector
 install_exercise_icons
 
 # Do this again, just in case any of the apt-get installs nuked it.
-install_root_config_files_toby
+install_root_config_files # from setup_fns.sh
 
 # Finally, we can start the crontab!
 install_crontab    # from setup_fns.sh
 
-echo "Starting daemons"
 start_daemons    # from setup_fns.sh
---
+++ phabricator/setup.sh
@@ -0,0 +1,211 @@
+#!/bin/sh
+
+# This has files that are used on our phabricator machine, that runs
+# our phabricator server on ec2.
+#
+# This sets up packages needed on an EC2 machine for phabricator.
+# This is based on an Ubuntu12.04 AMI.  It is idempotent.
+#
+# This should be run in the home directory of the role account of the
+# user that is to run phabricator.  That user should be able to sudo
+# to root.
+#
+# NOTE: This script, along with some of the data files in this
+# directory (notably etc/fstab), assume that a 'data' disk has been
+# attached to this ec2 instance as sdf (aka xvdf), and that's it's
+# been formatted with xfs.
+#
+# WARNING: you should definitely be careful running it the first time,
+# and treat it more like a README than something you can just run and
+# forget.
+#
+# SECURITY
+# --------
+# Because we store all our company secrets in the datastore db, this
+# ec2 machine is more concerned with security than most.  Here are
+# some of the security features of this install:
+#
+# 1) The ssh private key is protected by a password (TODO(csilvers))
+#    and access to that password will be limited to phabricator admins
+# 2) The ec2 firewall settings for this machine does not allow access
+#    to port 3306, so outsiders cannot access the mysql machine
+#    (even if mysql is set to listen to all IPs, which it probably isn't.)
+# 3) We set up nginx so all communication to phabricator happens over
+#    https.
+#
+# There are still some unresolved security issues:
+# A) The phabricator db is on an aws ebs drive, which an aws admin
+#    can access even without permissions.
+# B) Likewise, we back up the phabricator db to s3 every night, which
+#    someone with appropriate aws permissions could access.
+#
+# Security feature (2) means that we cannot install the phabricator
+# slaves on a separate machine, since they would not be able to
+# talk to the db.  If we wanted to do that, we'd have to be much
+# more careful about network permissions, mysql passwords, etc.
+
+# Bail on any errors
+set -e
+
+CONFIG_DIR="$HOME/aws-config/phabricator"
+. "$HOME/aws-config/shared/setup_fns.sh"
+
+
+install_ec2_tools() {
+    # Activate the multiverse!  Needed for ec2-api-tools
+    activate_multiverse       # from setup_fns.sh
+    sudo apt-get install -y ec2-api-tools
+    mkdir -p "$HOME/aws"
+    if [ ! -s "$HOME/aws/pk-backup-role-account.pem" ]; then
+        echo "Copy the pk-backup-role-account.pem and cert-backup-role-account.pem"
+        echo "files from dropbox to $HOME/aws:"
+        echo "   https://www.dropbox.com/home/Khan%20Academy%20All%20Staff/Secrets"
+        echo "Also, make sure there is an IAM user called 'backup-role-account"
+        echo "with permissions from 'backup-role-account-permissions'."
+        echo "Then hit enter to continue"
+        read prompt
+    fi
+}
+
+install_repositories() {
+    echo "Syncing internal-webserver codebase"
+    sudo apt-get install -y git
+    git clone git://github.com/Khan/aws-config || \
+        ( cd aws-config && git pull && git submodule update --init --recursive )
+    # TODO(csilvers): move phabricator stuff to a separate phabricator repo.
+    git clone git://github.com/Khan/internal-webserver || \
+        ( cd internal-webserver && git pull && git submodule update --init --recursive )
+}
+
+install_user_config_files() {
+    echo "Creating logs directory (for webserver logs)"
+    sudo mkdir -p /opt/logs
+    sudo chmod 1777 /opt/logs
+    sudo chown www-data.www-data /opt/logs
+    ln -snf /opt/logs "$HOME/logs"
+    ln -snf /var/log/nginx/error.log "$HOME/logs/nginx-error.log"
+}
+
+install_phabricator() {
+    echo "Installing packages: Phabricator"
+    sudo mkdir -p /opt/mysql_data
+    ln -snf /opt/mysql_data "$HOME/mysql_data"
+    sudo apt-get install -y git mercurial
+    sudo apt-get install -y make
+    # The envvar here keeps apt-get from prompting for a password.
+    # (If we wanted to give the root user a password, we'd do that here.)
+    sudo env DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
+    sudo apt-get install -y php5 php5-mysql php5-cgi php5-fpm php5-gd php5-curl
+    sudo apt-get install -y libpcre3-dev php-pear    # needed to install apc
+    # php is dog-slow without APC
+    pecl list | grep -q APC || yes "" | sudo pecl install apc
+    sudo pip install pygments                      # for syntax highlighting
+    sudo ln -snf /usr/local/bin/pygmentize /usr/bin/
+    mkdir -p "$HOME/internal-webserver/phabricator/support/bin"
+    cat <<EOF >"$HOME/internal-webserver/phabricator/support/bin/README"
+Instead of putting binaries that phabricator needs in your $PATH, you can
+put symlinks to them here.  (This directory is in your phabricator-path.)
+EOF
+    if [ ! -s "/usr/lib/php5/20090626/xhprof.so"]; then
+        ( cd /var/tmp
+          rm -rf xhprof-0.9.2.tgz xhprof-0.9.2
+          wget http://pecl.php.net/get/xhprof-0.9.2.tgz
+          tar xfz xhprof-0.9.2.tgz
+          rm xhprof-0.9.2.tgz
+          cd xhprof-0.9.2/extension
+          phpize
+          ./configure
+          make
+          sudo make install
+        )
+    fi
+    echo "SELECT User FROM mysql.user" \
+        | mysql --user=root mysql \
+        | grep -q phabricator \
+    || echo "CREATE USER 'phabricator'@'localhost' IDENTIFIED BY 'codereview'; GRANT ALL PRIVILEGES ON *.* TO 'phabricator'@'localhost' WITH GRANT OPTION;" \
+        | mysql --user=root mysql
+
+    # Just in case phabricator is already running (we want to be idempotent!)
+    PHABRICATOR_ENV=khan "$HOME/internal-webserver/phabricator/bin/phd" stop
+    sudo service nginx stop
+    sudo service php5-fpm stop
+
+    env PHABRICATOR_ENV=khan \
+        internal-webserver/phabricator/bin/storage --force upgrade
+
+    # TODO(csilvers): automate entering this info:
+    cat <<EOF
+Enter this info:
+    * username: admin
+    * real name: Admin Admin
+    * email: toby-admin+phabricator@khanacademy.org
+    * password: <see secrets.py>
+    * system agent: n
+    * admin: y
+EOF
+    env PHABRICATOR_ENV=khan internal-webserver/phabricator/bin/accountadmin
+
+    # Set up the security token
+    "$HOME/internal-webserver/arcanist/bin/arc" install-certificate \
+        https://phabricator.khanacademy.org/api/
+
+    # Store repositories and files on the same disk as mysql so that
+    # they get backed up alongside the database data.
+    mkdir -p "$HOME/phabricator"
+    sudo mkdir -p /opt/phabricator_repositories
+    sudo chmod -R a+rX /opt/phabricator_repositories
+    sudo chown -R www-data /opt/phabricator_repositories
+    ln -snf /opt/phabricator_repositories "$HOME/phabricator/repositories"
+
+    sudo mkdir -p /opt/phabricator_files
+    sudo chmod -R a+rX /opt/phabricator_files
+    sudo chown -R www-data /opt/phabricator_files
+    ln -snf /opt/phabricator_files "$HOME/phabricator/files"
+
+    python "$HOME/internal-webserver/update_phabricator_repositories.py" -v \
+        "$HOME/phabricator/repositories"
+
+    # Start the daemons.
+    sudo mkdir -p /var/tmp/phd/log
+    sudo chown -R ubuntu /var/tmp/phd
+    sudo service php5-fpm start
+    sudo service nginx start
+    PHABRICATOR_ENV=khan "$HOME/internal-webserver/phabricator/bin/phd" start
+
+    # TODO(csilvers): automate this.
+    cat <<EOF
+To finish phabricator installation:
+1) Visit http://phabricator.khanacademy.org, sign in as admin, visit
+   http://phabricator.khanacademy.org/auth/config/new/, and follow
+   the directions to enable Google OAuth.
+2) Sign out, then sign in again via oauth, create a new account for
+   yourself, sign out, sign in as admin, and visit
+   http://phabricator.khanacademy.org/people/edit/2/role/
+   to make yourself an admin.
+3) On AWS's route53 (or whatever), add phabricator-files.khanacademy.org
+   as a CNAME to phabricator.khanacademy.org.
+4) Copy ka-wild-13.* from
+   https://www.dropbox.com/home/Khan%20Academy%20All%20Staff/Secrets
+   to /etc/nginx (as root) and chmod 600 /etc/nginx/ka-wild-13.*.
+EOF
+echo "Hit enter when this is done:"
+read prompt
+}
+
+
+update_aws_config_env     # from setup_fns.sh
+install_basic_packages    # from setup_fns.sh
+install_ec2_tools
+install_repositories
+install_root_config_files # from setup_fns.sh
+install_user_config_files
+install_nginx             # from setup_fns.sh
+install_phabricator
+
+# Do this again, just in case any of the apt-get installs nuked it.
+install_root_config_files_toby # from setup_fns.sh
+
+# Finally, we can start the crontab!
+install_crontab    # from setup_fns.sh
+
+start_daemons    # from setup_fns.sh
