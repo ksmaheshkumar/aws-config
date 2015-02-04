6cbc38ddfc424eb8881ea44da6eb964366b084cb
Craig Silverstein <csilvers@khanacademy.org>
2015-02-04 13:04:32 -0800

    Have cronbase include the full path: we just want to remove the .tpl
    I also added a missing sudo.
    
    Auditors: chris
    
    Test plan:
    Ran this command manually on jenkins and did:
        % cat /etc/cron.d/oom-detector
        # NOTE: make sure the google group is set up to have root@khanacademy.org
        #       as a member, or this mail will bounce!
        # 'hostname' should be 'basename `cat /etc/mailname` .khanacademy.org'
        MAILTO = jenkins-admin+crontab@khanacademy.org
        PATH = /usr/local/bin:/usr/bin:/bin
    
        # Every minute, check if we had an OOM event, and if so send mail.
        # (We 'send mail' by echoing the output, which MAILTO above turns into
        # mail.)  The OOM message in kern.log starts with '<something> invoked
        # oom-killer' and ends with 'Killed process <something>'.  This will
        # print all OOM messages in the logfile, even rather old ones.
        # TODO(csilvers): only print OOM's that ended within the last minute.
        * * * * *       root    find /var/log -name 'kern.log.*' -mmin -2 -print0 | xargs -0 -r sed -ne '/invoked oom-killer/,/Killed process/p'
    
    The MAILTO looks exactly right.
    
    I waited a minute and did 'tail /var/log/syslog' and saw:
       Feb  4 13:04:01 jenkins CRON[6350]: (root) CMD (   find /var/log -name 'kern.log.*' -mmin -2 -print0 | xargs -0 -r sed -ne '/invoked oom-killer/,/Killed process/p')
    
    which also seems right to me -- I guess the command is running!

--- shared/setup_fns.sh
+++ shared/setup_fns.sh
@@ -156,13 +156,13 @@ install_root_config_files() {
             rm -f "`_to_fs "$cronfile"`"
             # If it's a .tpl file, expand the macro first.
             if expr "$cronfile" : ".*\.tpl$" >/dev/null; then
-                cronbase=`basename "$cronfile" .tpl`
+                cronbase=`echo "$cronfile" | sed "s/.tpl$//"`
                 # Right now we only have one var we need to expand.
                 mailname=`cat /etc/mailname`
                 hostname=`basename "$mailname" .khanacademy.org`
                 sed "s/{{hostname}}/$hostname/g" "$cronfile" \
                     | sudo tee "`_to_fs "$cronbase"`" >/dev/null
-                chmod 755 "`_to_fs "$cronbase"`"
+                sudo chmod 755 "`_to_fs "$cronbase"`"
             else
                 sudo install -m755 "$cronfile" "`_to_fs "$cronfile"`"
             fi
